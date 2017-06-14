1ea00a40480b423aadb4160c4d42da1b88e9938f
juansebascb <cal13004@byui.edu>
2017-06-13 20:28:22 -0400

    Added a multiline text, the id is sharedText

--- app/src/main/java/com/pyle/syncnote/MainMenu.java
+++ app/src/main/java/com/pyle/syncnote/MainMenu.java
@@ -11,7 +11,7 @@ public class MainMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_text_editor);
 
         NetworkCallback callback = new NetworkCallback();
 
