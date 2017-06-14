6c866f88c7f7cf71fead43cb62c68bec306cc595
juansebascb <cal13004@byui.edu>
2017-06-13 20:50:53 -0400

    Added the files that I had separated

---
+++ app/src/main/java/com/pyle/syncnote/activityTextEditor.java
@@ -0,0 +1,13 @@
+package com.pyle.syncnote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activityTextEditor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_editor);
+    }
+}
